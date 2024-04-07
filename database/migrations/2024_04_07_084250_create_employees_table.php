861486e07a0070bdfcb63e958c677310b186a63a
Nikhil Sarvaiya <nikhilsarvaiya@Nikhils-MacBook-Pro.local>
2024-04-07 22:44:24 +0530

    setup laravel employee project

---
+++ database/migrations/2024_04_07_084250_create_employees_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name')->nullable();
+            $table->string('last_name')->nullable();
+            $table->string('email')->nullable();
+            $table->string('country_code')->nullable();
+            $table->string('mobile')->nullable();
+            $table->text('address')->nullable();
+            $table->tinyInteger('gender')->comment('0=male,1=female');
+            $table->string('hobby')->nullable();
+            $table->text('image')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employees');
+    }
+};
